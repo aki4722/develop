37cea94e442981448f0180b1d404047aeb5e3e59
秋元裕志 <akimotohiroshi@MacBookPro.local>
2017-09-18 08:23:02 +0900

    new add

--- unity_test05/Assets/BulletController.cs
+++ unity_test05/Assets/BulletController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class BulletController : MonoBehaviour {
+	public GameObject explosionPrefab;   //爆発エフェクトのPrefab
 	void Update () {
 		transform.Translate (0, 0.2f, 0);
 
@@ -10,6 +11,8 @@ public class BulletController : MonoBehaviour {
 		}
 	}
 	void OnTriggerEnter2D(Collider2D coll) {
+		// 衝突したときにスコアを更新する
+		GameObject.Find ("Canvas").GetComponent<UIController> ().AddScore ();
 		// 爆発エフェクトを生成する	
 		Instantiate (explosionPrefab, transform.position, Quaternion.identity);
 		Destroy (coll.gameObject);
