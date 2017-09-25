a91e69403ea46b2dd8434611ab1aa420ddbbd902
sra <z-akmoto@sra.co.jp>
2017-09-25 21:44:21 +0900

    add

---
+++ PlatformInputTest/Assets/Sample.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput; //←追加
+
+public class Sample : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		// 水平方向への入力( -1.0 ～ 1.0 )
+		Debug.Log( "Horizontal : " + CrossPlatformInputManager.GetAxisRaw( "Horizontal" ) );
+		// 垂直方向への入力( -1.0 ～ 1.0 )
+		Debug.Log( "Vertical : " + CrossPlatformInputManager.GetAxisRaw( "Vertical" ) );
+
+	}
+}
